469d573df33cec0776313526654d4f7696f9d738
drujbanjo <drujbanjo@gmail.com>
2024-12-20 10:02:03 +0500

    (#1)

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useEffect, useState } from "react"
+import styles from "./page.module.css"
+
+export default function Home() {
+	const [currentData, setCurrentData] = useState<{ message: string }>()
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await fetch("http://localhost:4201/")
+			const data = await res.json()
+			setCurrentData(data)
+		}
+		fetchData()
+	})
+	return (
+		<div className={styles.page}>
+			{currentData && <p>{currentData.message}</p>}
+		</div>
+	)
+}
