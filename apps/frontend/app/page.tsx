97741a73d70b59b4abce12f4f77d8f1d78f65179
drujbanjo <drujbanjo@gmail.com>
2024-12-20 10:12:27 +0500

    (#4)

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 			setCurrentData(data)
 		}
 		fetchData()
-	})
+	}, [])
 	return (
 		<div className={styles.page}>
 			{currentData && <p>{currentData.message}</p>}
